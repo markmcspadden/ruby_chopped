16d6f89cddc246712dae10af09a4e57a060967cb
Mike Vincent <mike@cryingwhilecoding.com>
2011-08-19 13:30:34 -0500

    Use ruby optparse lib for options
    Also....
    moved generation logic into method of RubyChopped module
    extend RubyChopped with self for easy class/instant accessability
    added force option to overwrite existing project (do-over)

--- lib/ruby_chopped.rb
+++ lib/ruby_chopped.rb
@@ -1,8 +1,37 @@
 require 'rest-client'
+require 'fileutils'
 require 'json'
 
 module RubyChopped
-  def self.gemfile_array
+  extend self
+
+  def create(opts={})
+    folder = opts[:name]
+    if File.exist?(folder)
+      unless opts[:force]
+        puts "#{folder} already exists, use -f/--force option to recreate it"
+        exit 1
+      end
+    end
+
+    puts "Creating #{folder} basket..."
+    FileUtils.mkdir_p("#{folder}/lib")
+
+    File.open("#{folder}/lib/#{folder}.rb", "w") {|f| f.puts "# Where the magic happens!" }
+
+    File.open("#{folder}/Gemfile", "wb+") do |f|
+      f << RubyChopped.gemfile_string
+    end
+    
+    puts ""
+    puts "Your basket is ready. Open the Gemfile to see what you'll be working with today."
+
+    puts ""
+    puts "You'll want to cd into #{folder} and run 'bundle install' first"
+    puts "Enjoy!"
+  end
+
+  def gemfile_array
     gas = []
     gas << "source \"http://rubygems.org\""
     gas << ""
@@ -25,16 +54,16 @@ module RubyChopped
     gas
   end
   
-  def self.gemfile_string
+  def gemfile_string
     gemfile_array.join("\n")
   end
     
-  def self.random_gems(limit=2)
+  def random_gems(limit=2)
     gems = fetch_gems
     gems = pick_gems(gems, limit)
   end
   
-  def self.pick_gems(gems, limit)
+  def pick_gems(gems, limit)
     limit.to_i.times.collect do 
       g = gems.delete_at(rand(gems.size)) 
 
@@ -47,7 +76,7 @@ module RubyChopped
     end
   end
   
-  def self.fetch_gems
+  def fetch_gems
     gems_json = JSON.parse(RestClient.get("http://rubygems.org/api/v1/downloads/top.json", :accepts => :json))
     gems = gems_json["gems"]    
     gems
